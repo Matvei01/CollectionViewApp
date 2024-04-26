f0ab6ba169edd7952498fd5ea64a6ccdae955239
Matvei <khlestov.matvei@mail.ru>
2024-04-27 01:36:12 +0500

    Created base project, removed storyboard references

---
+++ CollectionViewApp/CollectionViewApp/Controllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  CollectionViewApp
+//
+//  Created by Matvei Khlestov on 27.04.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ CollectionViewApp/CollectionViewApp/Views/CustomViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomViewCell.swift
+//  CollectionViewApp
+//
+//  Created by Matvei Khlestov on 27.04.2024.
+//
+
+import UIKit
+
+final class CustomViewCell: UICollectionViewCell {
+    
+}
